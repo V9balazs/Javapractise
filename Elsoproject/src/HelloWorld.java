1678e696bdf06f6766976d5acaf4ccdbeb789a59
Varga Balazs <9balazs@gmail.com>
2021-01-04 11:08:34 +0100

    The source

---
+++ Elsoproject/src/HelloWorld.java
@@ -0,0 +1,200 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+
+		// primitív tipusú változók;
+//		byte a = 127;
+//		short b = 100;
+//		int c = 100;
+//		long d = 100;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//			
+//		float e = 12.5f;
+//		double f = 12.5;
+//		boolean g;
+//		char h;
+//		
+//		int i = 10;
+//		System.out.println(i);
+//		i = 15;
+//		System.out.println(i);
+//		
+//		int apples = 7;
+//		int pears = 12;
+//		System.out.println(apples);
+//		System.out.println(pears);
+//		int fruits = apples + pears;
+//		System.out.println(fruits);
+//		
+//		int numberofboxes = 100;
+//		int numberofboxessold = 17;
+//		int numberofboxesremaining = numberofboxes - numberofboxessold;
+//		System.out.println(numberofboxesremaining);
+//		
+//		int j = 17;
+//		int z = 5;
+//		int k = j % z;
+//		System.out.println(k);
+//		
+//		boolean b1 = true;
+//		boolean b2 = false;
+//		boolean result = b1 && b2;
+//		System.out.println(result);
+
+//		System.out.println(150 % 4);
+//		System.out.println(151 % 4);
+//		System.out.println(152 % 4);
+//		System.out.println(153 % 4);
+//		System.out.println(154 % 4);
+//		System.out.println(155 % 4);
+//		System.out.println(156 % 4);
+//		System.out.println(157 % 4);
+
+		// referencia tipusú változók
+//		int number = (2 + 30) * 4;
+//		System.out.println(number);
+//		boolean b = number + 10 < 100;
+//		System.out.println(b);
+
+//		int i = 100;
+//		int j = i++ + ++i;
+//		System.out.println(j);
+
+//		boolean b1 = false;
+//		boolean b2 = true;
+//		boolean result = b1 && b2;
+//		System.out.println(b1);
+//		System.out.println(b2);
+//		System.out.println(result);
+
+//		int i = 100;
+//		boolean finalresult = b1 && i++ < 1000;
+//		System.out.println(finalresult);
+//		System.out.println(i);
+
+//		boolean b1 = true;
+//		boolean b2 = true;
+//		boolean b3 = true;		
+//		boolean result = b1 || b2 && b3;
+//		System.out.println(result);
+
+//		int i = 54;
+//		System.out.println(i % 2 == 0 ? "páros" : "páratlan");
+
+//		int[] numbers = new int[10];
+//		numbers[2] = 10;
+//		System.out.println(Arrays.toString(numbers));
+
+//		boolean a = true;
+//		boolean b = false;
+//		System.out.println(a^b ?"Ez igaz" :"Ez hamis");
+
+//		int[][] numbers = new int[5][];
+//		System.out.println();
+//		int[]data1 = new int[4];
+//		int[]data2 = new int[7];
+//		int[]data3 = new int[10];
+//		int[]data4 = new int[2];
+//		numbers[0] = data1;
+//		numbers[1] = data2;
+//		numbers[2] = data3;
+//		numbers[3] = data4;
+
+//		String[] names = new String[4];
+//		String[][] names2 = {{"Aaron", "John"}, {"Blaize", "Dave"}};
+
+//		int number = -1;
+//		if (number < 0)
+//		{
+//			System.out.println("A szám negatív");
+//		} else if (number > 0)
+//		{
+//			System.out.println("A szám pozitív");
+//		} else
+//		{
+//			System.out.println("A szám nulla");
+//		}
+
+//		int dayofweek = 3;
+//		switch (dayofweek)
+//		{
+//		case 1: System.out.println("hétfő");break;
+//		case 2: System.out.println("kedd");break;
+//		case 3: System.out.println("szerda");break;
+//		case 4: System.out.println("csütörtök");break;
+//		case 5: System.out.println("péntek");break;
+//		default: System.out.println("ismeretlen");
+//		};
+
+//		 String[] fruits = {"alma","körte","szilva"};
+//		for (String fruit : fruits) {
+//			System.out.println(fruit);
+//		}
+//		
+//		int[] winninglotterynumbers = {43,66,10,11,42};
+//		for (int winningnumbers : winninglotterynumbers) {
+//			System.out.println(winningnumbers);
+//		}
+//		
+//		boolean[] answers = {true, true,false,false,true};
+//		for (boolean answer : answers) {
+//			System.out.println("a válasz : " + answer);
+//		}
+
+//		int i = 100;
+//		do
+//		{
+//			i *= 2;
+//			System.out.println(i);
+//		}while(i < 1000);
+
+//		String[] daysofweek = {"hétfő", "kedd", "szerda", "csütörtök", "péntek" ,"szombat", "vasárnap"};
+//		String[] mealsofday = {"reggeli", "ebéd", "vacsora"};
+//		outer: for (String day : daysofweek) {
+//			System.out.println(day);
+//			inner: for (String meal : mealsofday) {
+//				if (day.equals("csütörtök")) {
+//					break outer;
+//				}
+//				System.out.println(" " + meal);
+//			}
+//		}
+
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("Kérem, adjon meg egy számot: ");
+//		int number = scanner.nextInt();
+//		scanner.nextLine();
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+	}
+
+}
