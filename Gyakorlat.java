d8f34e844ea1c9b23a397f945e9bf6854556ee5a
Varga Balazs <9balazs@gmail.com>
2021-01-04 14:25:21 +0100

    Sources

---
+++ Gyakorlat.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Gyakorlat {
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Kérem, adja meg a pénzösszeget: ");
+		int moneyamout = scanner.nextInt();
+		System.out.println("A megadott pénzösszeg: " + moneyamout);
+		int[] currencies = {20_000, 10_000, 5_000, 2_000, 1_000, 500, 200, 100, 50, 20, 10, 5};
+		int remainingamount = moneyamout;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
